0b8f2bb8dff472a8fd5d8c59e5af0a264c5d1a96
Abdulbaki <Salausmart132@gmail.com>
2025-06-10 21:37:31 +0300

    create and get tracks endpoints

--- src/main/java/com/ibnbaqqi/fitnessTracker/controller/TrackerController.java
+++ src/main/java/com/ibnbaqqi/fitnessTracker/controller/TrackerController.java
@@ -1,21 +1,32 @@
 package com.ibnbaqqi.fitnessTracker.controller;
 
-import com.ibnbaqqi.fitnessTracker.dto.createTrackerRequest;
+import com.ibnbaqqi.fitnessTracker.dto.TrackerDto;
+import com.ibnbaqqi.fitnessTracker.dto.CreateTrackerRequest;
+import com.ibnbaqqi.fitnessTracker.service.TrackerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class TrackerController {
 
+    private final TrackerService trackerService;
+
+    public TrackerController(TrackerService trackerService) {
+        this.trackerService = trackerService;
+    }
+
     @PostMapping("/tracker")
-    public ResponseEntity<?> createTrack(@RequestBody createTrackerRequest request) {
+    public ResponseEntity<?> createTrack(@RequestBody CreateTrackerRequest request) {
+        trackerService.createTracker(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/tracker")
-    public ResponseEntity<?> getTrack() {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public List<TrackerDto> getTrack() {
+        return trackerService.getTracker();
     }
 }
